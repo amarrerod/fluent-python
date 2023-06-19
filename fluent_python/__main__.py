65594d7065636586733610dc60b8a338c5c8cc2c
amarrerod <alemarrerodiaz@gmail.com>
2023-06-19 10:40:43 +0100

    🎉 Starts Chapter 4

--- fluent_python/__main__.py
+++ fluent_python/__main__.py
@@ -4,8 +4,10 @@
 from .chapter_1_data_model.__main__ import main as chpt_1_main
 from .chapter_2_arrays.__main__ import main as chpt_2_main
 from .chapter_3_dicts_and_sets.__main__ import main as chpt_3_main
+from .chapter_4_unicode_vs_bytes.__main__ import main as chpt_4_main
 
 if __name__ == "__main__":
     chpt_1_main()
     chpt_2_main()
     chpt_3_main()
+    chpt_4_main()
