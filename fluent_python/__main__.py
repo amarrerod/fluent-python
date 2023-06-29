4d8240f2f400801dd903b906fc4d17ad2b5e4cc2
amarrerod <alemarrerodiaz@gmail.com>
2023-06-29 09:52:40 +0100

    🎉 Starts a new chapter

--- fluent_python/__main__.py
+++ fluent_python/__main__.py
@@ -5,9 +5,11 @@ from .chapter_1_data_model.__main__ import main as chpt_1_main
 from .chapter_2_arrays.__main__ import main as chpt_2_main
 from .chapter_3_dicts_and_sets.__main__ import main as chpt_3_main
 from .chapter_4_unicode_vs_bytes.__main__ import main as chpt_4_main
+from .chapter_5_dataclass.__main__ import main as chpt_5_main
 
 if __name__ == "__main__":
     chpt_1_main()
     chpt_2_main()
     chpt_3_main()
     chpt_4_main()
+    chpt_5_main()
