40500a7cad1f471f4c189da74d1c46c6be9d4e50
amarrerod <alemarrerodiaz@gmail.com>
2023-10-19 12:15:56 +0100

    🚧 Working on chapter 11

--- fluent_python/chapter_11_pythonic_object/__main__.py
+++ fluent_python/chapter_11_pythonic_object/__main__.py
@@ -1,5 +1,18 @@
+from .vector2d_v0 import Vector2d
+
+
 def main():
-    pass
+    v1 = Vector2d(3, 4)
+    print(v1.x, v1.y)
+    x, y = v1
+    print(f"X: {x}, Y: {y}")
+    print(f"Vector {v1}")
+
+    octets = bytes(v1)
+    print(octets)
+    v2 = Vector2d.frombytes(octets)
+    print(v2)
+    print(v1 == v2)
 
 
 if __name__ == "__main__":
