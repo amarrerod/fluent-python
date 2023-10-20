a6c17adf2b727fb424c5ca3ca1f45314e830663f
amarrerod <alemarrerodiaz@gmail.com>
2023-10-20 10:41:21 +0100

    🚀 Chapter 11 done

--- fluent_python/chapter_11_pythonic_object/__main__.py
+++ fluent_python/chapter_11_pythonic_object/__main__.py
@@ -1,6 +1,22 @@
 from .vector2d_v0 import Vector2d
 
 
+def positional_pattern_demo(v: Vector2d) -> None:
+    message = ""
+    match v:
+        case Vector2d(0, 0):
+            message = f"{v!r} is null"
+        case Vector2d(0):
+            message = f"{v!r} is vertical"
+        case Vector2d(_, 0):
+            message = f"{v!r} is horizontal"
+        case Vector2d(x, y) if x == y:
+            message = f"{v!r} is diagonal"
+        case _:
+            message = f"{v!r} is awesome"
+    print(message)
+
+
 def main():
     v1 = Vector2d(3, 4)
     print(v1.x, v1.y)
@@ -13,6 +29,22 @@ def main():
     v2 = Vector2d.frombytes(octets)
     print(v2)
     print(v1 == v2)
+
+    print(f"Regular format: {v1:.2f}")
+    print(f"Scientific notation format: {v1:.3e}")
+
+    print(f"Polar Regular format: {v1:.2fp}")
+    print(f"Polar Scientific notation format: {v1:.3ep}")
+
+    print(f"Complex representation {complex(v1)}")
+    v2 = Vector2d(3, -10)
+    print(f"Complex representation of negative vector: {complex(v2)}")
+
+    positional_pattern_demo(Vector2d(0, 0))
+    positional_pattern_demo(Vector2d(10, 0))
+    positional_pattern_demo(Vector2d(0, 10))
+    positional_pattern_demo(Vector2d(10, 10))
+    positional_pattern_demo(Vector2d(40, 30))
 
 
 if __name__ == "__main__":
--- fluent_python/chapter_11_pythonic_object/vector2d_v0.py
+++ fluent_python/chapter_11_pythonic_object/vector2d_v0.py
@@ -4,10 +4,29 @@ import math
 
 class Vector2d:
     typecode = "d"
+    # We include this to allow positional pattern matching
+    # in match statements
+    __match_args__ = ("x", "y")
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        """
+        Hacemos x e y componentes de solo lectura
+        para poder conseguir que Vector2d sea hashable
+        """
+        return self.__x
+
+    @property
+    def y(self):
+        """
+        Hacemos x e y componentes de solo lectura
+        para poder conseguir que Vector2d sea hashable
+        """
+        return self.__y
 
     def __iter__(self):
         # return (i for i in (self.x, self.y))
@@ -21,6 +40,9 @@ class Vector2d:
     def __str__(self):
         return str(tuple(self))
 
+    def __complex__(self):
+        return complex("{0}{1:+}j".format(*self))
+
     def __bytes__(self):
         return bytes([ord(self.typecode)]) + bytes(array(self.typecode, self))
 
@@ -30,11 +52,33 @@ class Vector2d:
     def __abs__(self):
         return math.hypot(self.x, self.y)
 
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
     def __bool__(self):
         return bool(abs(self))
+
+    def __hash__(self):
+        """
+        The __hash__ special method documentation
+        suggest computing the hash of a tuple with the
+        components
+        """
+        return hash((self.x, self.y))
 
     @classmethod
     def frombytes(cls, octets):
         typecode = chr(octets[0])
         memv = memoryview(octets[1:]).cast(typecode)
         return cls(*memv)
+
+    def __format__(self, fmt_spec=""):
+        if fmt_spec.endswith("p"):
+            fmt_spec = fmt_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = "<{}, {}>"
+        else:
+            coords = self
+            outer_fmt = "({}, {})"
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(*components)
